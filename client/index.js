5ab957895eafa21d237d5f80ade238141297f240
Aylan Mello <aylanismello@gmail.com>
2017-10-27 17:51:07 -0700

    changed IP to droplet

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 var io = require('socket.io-client');
-const url = process.env.NODE_ENV === 'production' ? 'http://104.236.137.60' : 'http://localhost:3000';
+const HEROKU_DROPLET_URL = 'http://162.243.144.110';
+const url = process.env.NODE_ENV === 'production' ? HEROKU_DROPLET_URL : 'http://localhost:3000';
 
 var socket = io.connect(url, {reconnect: true});
 
