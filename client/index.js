0dcfd7188235d8c36e509b9420c03356978608c7
Aylan Mello <aylanismello@gmail.com>
2017-10-27 20:27:58 -0700

    prepping to receive audio data

--- client/index.js
+++ client/index.js
@@ -1,11 +1,25 @@
-var io = require('socket.io-client');
+const io = require('socket.io-client');
+const ss = require('socket.io-stream');
+const fs = require('fs');
 const HEROKU_DROPLET_URL = 'http://162.243.144.110';
 const url = process.env.NODE_ENV === 'production' ? HEROKU_DROPLET_URL : 'http://localhost:3000';
 
-var socket = io.connect(url, {reconnect: true});
+const socket = io.connect(url, {reconnect: true});
 
 socket.on('connect', () => {
   console.log('connected!');
+
+  ss(socket).on('audio', function(audioStream, data) {
+    console.log('got audio from server!');
+    // console.log('got audio audioStream, piping stream to clients');
+    // ss(socket).emit('audio', relayedAudioStream, { name: 'whatever'})
+    // audioStream.pipe(relayedAudioStream);
+    // // fs.createReadStream(filename).pipe(stream)
+
+
+    // THIS IS THE OLD WORKINGCODE
+    audioStream.pipe(fs.createWriteStream('./yo.wav'));
+  });
 });
 
 socket.on('tick', (data) => {
